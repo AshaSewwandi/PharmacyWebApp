bf12c5fc6c0115eea657251f4d8f2a1bcd35f36c
AshaSewwandi <ashasewwandihl@gmail.com>
2021-10-13 16:55:23 +0530

    Sixth Commit

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-/////
+//----Customer----
 Route::get('/Addcustomer/{id}', [App\Http\Controllers\CustomerController::class, 'create']);
 Route::post('/saveCustomer', [App\Http\Controllers\CustomerController::class, 'store'])->name('/saveCustomer');
 Route::get('/editcustomer/{CustomerID}', [App\Http\Controllers\CustomerController::class, 'edit']);
@@ -29,8 +29,10 @@ Route::post('/editCustomer', [App\Http\Controllers\CustomerController::class, 'u
 Route::get('/daletecustomer/{CustomerID}', [App\Http\Controllers\CustomerController::class, 'destroy']);
 Route::get('/viewcustomer', [App\Http\Controllers\CustomerController::class, 'show']);
 
+//-----Item-----
 Route::get('/Additem/{id}', [App\Http\Controllers\ItemController::class, 'create']);
 Route::post('/saveItem', [App\Http\Controllers\ItemController::class, 'store'])->name('/saveItem');
 Route::get('/viewitem', [App\Http\Controllers\ItemController::class, 'show']);
 Route::get('/editItem/{ItemID}', [App\Http\Controllers\ItemController::class, 'edit']);
 Route::get('/deleteItem/{ItemID}', [App\Http\Controllers\ItemController::class, 'destroy']);
+Route::post('/saveItem', [App\Http\Controllers\ItemController::class, 'update'])->name('/saveItem');
